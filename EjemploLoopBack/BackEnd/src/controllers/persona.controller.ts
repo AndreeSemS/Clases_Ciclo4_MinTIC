1dfab06fa33600049001f762270a8b00d6f0843b
andree <baladinom@unal.edu.co>
2021-11-18 18:18:45 -0500

    Validación con Token e implementación de estrategia para admin

--- EjemploLoopBack/BackEnd/src/controllers/persona.controller.ts
+++ EjemploLoopBack/BackEnd/src/controllers/persona.controller.ts
@@ -17,10 +17,12 @@ import {
   del,
   requestBody,
   response,
+  HttpErrors,
 } from '@loopback/rest';
-import {Persona} from '../models';
+import {Credenciales, Persona} from '../models';
 import {PersonaRepository} from '../repositories';
 import {AutenticacionService} from '../services';
+import {Llaves} from '../config/llaves';
 const fetch = require("node-fetch");
 
 export class PersonaController {
@@ -30,6 +32,32 @@ export class PersonaController {
     @service(AutenticacionService)
     public servicioAutenticacion:AutenticacionService,
   ) {}
+
+  @post('/identificarPersona', {
+    responses:{
+      '200':{
+        description: "identificación de usuarios"
+      }
+    }
+  })
+  async identificarPersona(
+    @requestBody() credenciales:Credenciales
+  ){
+    let p = await this.servicioAutenticacion.IdentificarPersona(credenciales.usuario, credenciales.clave);
+    if(p){
+      let token = this.servicioAutenticacion.GenerarTokenJWT(p);
+      return {
+        datos:{
+          nombre: p.nombres,
+          correo: p.correo,
+          id: p.id
+        },
+        tk:token
+      }
+    }else{
+      throw new HttpErrors[401]("Los datos suministrados no son válidos");
+    }
+  }
 
   @post('/personas')
   @response(200, {
@@ -60,7 +88,7 @@ export class PersonaController {
     let asunto = "Registro app Pedidos";
     let mensaje = `Hola ${persona.nombres}, su usuario es: ${persona.correo} y su contraseña es: ${clave}`;
 
-    fetch(`http://127.0.0.1:5000/email?correo=${destino}&asunto=${asunto}&mensaje=${mensaje}`)
+    fetch(`${Llaves.urlServicioNotificaciones}/email?correo=${destino}&asunto=${asunto}&mensaje=${mensaje}`)
     .then((data:any)=>{
       console.log(data);
     })
